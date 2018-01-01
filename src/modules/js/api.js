331aed2b7cb714efba63f38547a83a12588951ce
shenzekun <726664809@qq.com>
2018-01-01 21:08:19 +0800

    完成首页

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,9 +1,10 @@
 let url = {
-    hostLists: '/index/hotLists'
+    hostLists: '/index/hotLists',
+    banner: '/index/banner'
 }
 
 //开发环境与真实环境的切换
-let host = 'http://rap.taobao.org/mockjsdata/23334'
+let host = 'http://rap.taobao.org/mockjsdata/24170'
 
 for (let key in url) {
     if (url.hasOwnProperty(key)) {
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,20 +4,56 @@ import './index.css';
 import Vue from 'vue';
 import axios from 'axios';
 import url from 'js/api.js';
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll);
+
+import Foot from 'components/Foot.vue';
+import Swipe from 'components/Swipe.vue';
 
 let app = new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false, //可以加载
+    allLoaded: false,
+    bannerLists: null
   },
   created() {
-    axios
-      .post(url.hostLists, {
-        pageNum: 1,
-        pageSize: 6
-      })
-      .then(res => {
-        this.lists = res.data.lists;
-      });
+    this.getLists();
+    this.getBanner()
+  },
+  methods: {
+    getLists() {
+      if (this.allLoaded) return;
+      //是否在加载中
+      this.loading = true;
+      axios.post(url.hostLists, {
+          pageNum: this.pageNum,
+          pageSize: 6
+        })
+        .then(res => {
+          let currentList = res.data.lists
+          //判断当前的长度是否小于页面的长度
+          if (currentList.length < this.pageSize) this.allLoaded = true
+          if (this.lists) {
+            this.lists = this.lists.concat(currentList)
+          } else {
+            this.lists = currentList
+          }
+          this.loading = false
+          this.pageNum++
+        });
+    },
+    getBanner() {
+        axios.get(url.banner).then(res => {
+            this.bannerLists = res.data.lists
+        })
+    }
+  },
+  components: {
+      Foot,
+      Swipe
   }
 });
