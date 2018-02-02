f58bd8b152065f93d800ef83cb8a0b2ec5003dc1
shenzekun <726664809@qq.com>
2018-02-02 21:21:50 +0800

    modify cart

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,3 +1,28 @@
-import './cart.css'
-import './cart_base.css'
-import './cart_trade.css'
+import './cart.css';
+import './cart_base.css';
+import './cart_trade.css';
+
+import Vue from 'vue';
+import url from 'js/api.js';
+import axios from 'axios';
+import mixin from 'js/mixin.js';
+import qs from 'qs';
+
+new Vue({
+  el: '.container',
+  data: {
+    lists: null
+  },
+  computed: {},
+  created() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      axios.post(url.cartLists).then(res => {
+        this.lists = res.data.cartList;
+      });
+    }
+  },
+  mixins: [mixin]
+});
