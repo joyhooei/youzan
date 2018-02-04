bc5edd01915b7ff2cd4ed2c888cae85831910bcd
shenzekun <726664809@qq.com>
2018-02-04 13:03:53 +0800

    modify cart

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -12,8 +12,50 @@ new Vue({
   el: '.container',
   data: {
     lists: null,
+    total: 0,
+    editingShop: null,
+    editingShopIndex: -1
   },
-  computed: {},
+  computed: {
+    allSelected: {
+      get() {
+        if (this.lists && this.lists.length) {
+          return this.lists.every(shop => {
+            return shop.checked;
+          });
+        }
+        return false;
+      },
+      set(newValue) {
+        this.lists.forEach(shop => {
+          shop.checked = newValue;
+          shop.goodsList.forEach(good => {
+            good.checked = newValue;
+          });
+        });
+      }
+    },
+    selectLists() {
+      if (this.lists && this.lists.length) {
+        let arr = [],
+          total = 0;
+        this.lists.forEach(shop => {
+          shop.goodsList.forEach(good => {
+            if (good.checked) {
+              arr.push(good);
+              total += good.price * good.number;
+            }
+          });
+        });
+        this.total = total;
+        return arr;
+      }
+      return [];
+    },
+    removeLists() {
+      
+    }
+  },
   created() {
     this.getList();
   },
@@ -21,13 +63,45 @@ new Vue({
     getList() {
       axios.post(url.cartLists).then(res => {
         let lists = res.data.cartList;
-        lists.forEach(shop => {        
+        lists.forEach(shop => {
+          shop.checked = true;
+          shop.removeChecked = false;
+          shop.editing = false;
+          shop.editingMsg = '编辑';
           shop.goodsList.forEach(good => {
             good.checked = true;
-          })
+            good.removeChecked = false;
+          });
         });
         this.lists = lists;
       });
+    },
+    selectGood(shop, good) {
+      good.checked = !good.checked;
+      shop.checked = shop.goodsList.every(good => {
+        return good.checked;
+      });
+    },
+    selectShop(shop) {
+      shop.checked = !shop.checked;
+      shop.goodsList.forEach(good => {
+        good.checked = shop.checked;
+      });
+    },
+    selectAll() {
+      this.allSelected = !this.allSelected;
+    },
+    edit(shop, shopIndex) {
+      shop.editing = !shop.editing;
+      shop.editingMsg = shop.editing ? '完成' : '编辑';
+      this.lists.forEach((item, i) => {
+        if (shopIndex !== i) {
+          item.editing = false;
+          item.editingMsg = shop.editing ? '' : '完成';
+        }
+      });
+      this.editingShop = shop.editing ? shop : null;
+      this.editingShopIndex = shop.editing ? shopIndex : -1;
     }
   },
   mixins: [mixin]
