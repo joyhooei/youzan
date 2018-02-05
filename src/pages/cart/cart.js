8ba4abd73a3e435331baed323f014ea6d543adaf
shenzekun <726664809@qq.com>
2018-02-05 13:15:47 +0800

    modify cart

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -7,6 +7,8 @@ import url from 'js/api.js';
 import axios from 'axios';
 import mixin from 'js/mixin.js';
 import qs from 'qs';
+import Velocity from 'velocity-animate';
+import Cart from 'js/cartService.js';
 
 new Vue({
   el: '.container',
@@ -35,6 +37,22 @@ new Vue({
         });
       }
     },
+    allRemoveSelected: {
+      get() {
+        if (this.editingShop) {
+          return this.editingShop.removeChecked;
+        }
+        return false;
+      },
+      set(newVal) {
+        if (this.editingShop) {
+          this.editingShop.removeChecked = newVal;
+          this.editingShop.goodsList.forEach(good => {
+            good.removeChecked = newVal;
+          });
+        }
+      }
+    },
     selectLists() {
       if (this.lists && this.lists.length) {
         let arr = [],
@@ -53,7 +71,16 @@ new Vue({
       return [];
     },
     removeLists() {
-      
+      if (this.editingShop) {
+        let arr = [];
+        this.editingShop.goodsList.forEach(good => {
+          if (good.removeChecked) {
+            arr.push(good);
+          }
+        });
+        return arr;
+      }
+      return [];
     }
   },
   created() {
@@ -77,19 +104,22 @@ new Vue({
       });
     },
     selectGood(shop, good) {
-      good.checked = !good.checked;
-      shop.checked = shop.goodsList.every(good => {
-        return good.checked;
+      let attr = this.editingShop ? 'removeChecked' : 'checked';
+      good[attr] = !good[attr];
+      shop[attr] = shop.goodsList.every(good => {
+        return good[attr];
       });
     },
     selectShop(shop) {
-      shop.checked = !shop.checked;
+      let attr = this.editingShop ? 'removeChecked' : 'checked';
+      shop[attr] = !shop[attr];
       shop.goodsList.forEach(good => {
-        good.checked = shop.checked;
+        good[attr] = shop[attr];
       });
     },
     selectAll() {
-      this.allSelected = !this.allSelected;
+      let attr = this.editingShop ? 'allRemoveSelected' : 'allSelected';
+      this[attr] = !this[attr];
     },
     edit(shop, shopIndex) {
       shop.editing = !shop.editing;
@@ -102,6 +132,68 @@ new Vue({
       });
       this.editingShop = shop.editing ? shop : null;
       this.editingShopIndex = shop.editing ? shopIndex : -1;
+    },
+    reduce(good) {
+      if (good.number === 1) return;
+      Cart.reduce(good.id).then(res => {
+        good.number--;
+      });
+    },
+    add(good) {
+      Cart.add(good.id).then(res => {
+        good.number++;
+      });
+    },
+    remove(shop, shopIndex, good, goodIndex) {
+      let ids = [];
+      this.removeLists.forEach(good => {
+        ids.push(good.id);
+      });
+      axios
+        .post(url.cartRemove, {
+          id: ids
+        })
+        .then(res => {
+          let arr = [];
+          this.editingShop.goodsList.forEach(good => {
+            let index = this.removeLists.findIndex(item => {
+              return item.id === good.id;
+            });
+            if (index === -1) {
+              arr.push(good);
+            }
+          });
+          if (arr.length) {
+            this.editingShop.goodsList = arr;
+          } else {
+            this.lists.splice(this.editingShopIndex, 1);
+            this.removeShop();
+          }
+        });
+    },
+    removeShop() {
+      this.editingShop = null;
+      this.editingShopIndex = -1;
+      this.lists.forEach(shop => {
+        shop.editing = false;
+        shop.editingMsg = '编辑';
+      });
+    },
+    start(e, good) {
+      good.startX = e.changedTouches[0].clientX;
+    },
+    end(e, shopIndex, good, goodIndex) {
+      let endX = e.changedTouches[0].clientX;
+      let left = '0';
+      if (good.startX - endX > 100) {
+        left = '-60px';
+      }
+      if (endX - good.startX > 100) {
+        left = '0px';
+      }
+      Velocity(this.$refs[`goods-${shopIndex}-${goodIndex}`], {
+        left
+      });
     }
   },
   mixins: [mixin]
