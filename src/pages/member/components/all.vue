70d1e036b33dbf4046e879f925a8fe008b47b8e6
shenzekun <726664809@qq.com>
2018-02-07 16:55:02 +0800

    modify form

--- src/pages/member/components/all.vue
+++ src/pages/member/components/all.vue
@@ -1,19 +1,22 @@
 <template>
    <div class="container " style="min-height: 597px;">
-    <div class="block-list address-list section section-first js-no-webview-block" >
-      <a class="block-item js-address-item address-item " @click="toEdit()">
-        <div class="address-title">tony 13112345678</div>
-        <p>广东省珠海市香洲区南方软件园</p>
+    <div class="block-list address-list section section-first js-no-webview-block"  v-if="lists&&lists.length">
+      <a class="block-item js-address-item address-item" 
+        @click="toEdit(list)" 
+        v-for="list in lists" 
+        :class="{'address-item-default':list.isDefault}"
+        :key="list.id">
+        <div class="address-title">{{list.name}} {{list.tel}}</div>
+        <p>{{list.provinceName}}{{list.cityName}}{{list.districtName}}{{list.address}}</p>
         <a class="address-edit">修改 </a>
       </a>
-      <a class="block-item js-address-item address-item address-item-default" @click="toEdit()">
-        <div class="address-title">tony 13112345678</div>
-        <p>北京市北京市东城区天安门</p>
-        <a class="address-edit">修改</a>
-      </a>
+    </div>
+    <div v-if="lists&&!lists.length">
+       没有地址，请添加
     </div>
     <div class="block stick-bottom-row center">
-      <router-link class="btn btn-blue js-no-webview-block js-add-address-btn" to="/address/form">
+      <router-link class="btn btn-blue js-no-webview-block js-add-address-btn"
+       :to="{name: 'form',query: {type: 'add'}}">
             新增地址
     </router-link>
     </div>
@@ -23,20 +26,24 @@
 <script>
 import Address from 'js/addressService.js';
 export default {
-  data () {
+  data() {
     return {
       lists: null
-    }
+    };
   },
-  created () {
+  created() {
     Address.list().then(res => {
       this.lists = res.data.lists;
-    }) 
+    });
   },
   methods: {
-    toEdit() {
+    toEdit(list) {
       this.$router.push({
-        path: '/address/form'
+        name: 'form',
+        query: {
+          type: 'edit',
+          instance: list
+        }
       });
     }
   }
